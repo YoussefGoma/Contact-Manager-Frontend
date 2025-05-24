94d6ebb374b2d7680ea98fc47ff284d01e839574
Youssef <uosef2111@gmail.com>
2025-05-25 00:08:06 +0300

    feat: add authentication logic and global styles for snack bars

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'contact-manager-frontend';
+
+  constructor(
+    private router: Router,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    const currentUser = this.authService.currentUserValue;
+    if (currentUser && currentUser.token) {
+      this.router.navigate(['/contacts']);
+    } else {
+      this.router.navigate(['/login']);
+    }
+  }
 }
