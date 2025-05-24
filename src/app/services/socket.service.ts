70df77209f9e8e0c7f7bf0f4cb7f8a111c59fd21
Youssef <uosef2111@gmail.com>
2025-05-25 00:16:40 +0300

    feat: add socket service for real-time updates

---
+++ src/app/services/socket.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { io, Socket } from 'socket.io-client';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SocketService {
+  private socket: Socket;
+
+  constructor() {
+    this.socket = io('http://localhost:3000');
+  }
+
+  listen(eventName: string): Observable<any> {
+    return new Observable((subscriber) => {
+      this.socket.on(eventName, (data) => {
+        subscriber.next(data);
+      });
+    });
+  }
+
+  emit(eventName: string, data: any) {
+    this.socket.emit(eventName, data);
+  }
+
+  disconnect() {
+    if (this.socket) {
+      this.socket.disconnect();
+    }
+  }
+}
